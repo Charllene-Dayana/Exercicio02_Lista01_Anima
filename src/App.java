bd0e1502a67e23befd5578cd0d704f6a4ba86007
unknown <charllenedayana.cc@gmail.com>
2023-04-08 13:04:24 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class main {
+    public static void main(String[] args) {
+
+        double Numero;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("me der um numero");
+        Numero = teclado.nextDouble();
+        System.out.println(Numero);
+    }
+}
